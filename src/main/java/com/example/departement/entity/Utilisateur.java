5b766b7ec830b3ae2c2f943f4a8a0619a41a324a
AnnaNdoye <ndoyeanna754@gmail.com>
2025-10-15 08:21:41 +0000

    connexion réussie modification profil réussie----- problème en vue

--- src/main/java/com/example/departement/entity/Utilisateur.java
+++ src/main/java/com/example/departement/entity/Utilisateur.java
@@ -2,8 +2,19 @@ package com.example.departement.entity;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "utilisateurs")
